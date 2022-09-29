dadd383a68aeaaaafdd4ac82f2e122f69e2bfaf4
jmanashti <114664269+jmanashti@users.noreply.github.com>
2022-09-29 10:52:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import pymssql
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,7 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello Javad"
+    conn = pymssql.connect(host=r'35.226.107.0',user=r'sqlserver',password=r'!Rankworks',database='rankworksgoogle')
+    Engagement_Rates='1';UI_Score='1';Last_Update=str(date.today())
+    cursor = conn.cursor()
+    cursor.execute('SELECT COUNT(*) FROM dbo.Twitter')
+    data = cursor.fetchall()
+    print('Number of Twitter Id Collected=',data)
+    conn.close()
+    return data
+
+
 
 
 if __name__ == "__main__":
